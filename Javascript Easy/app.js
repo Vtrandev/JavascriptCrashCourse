b601efe441eb5455d3e5c7018a7e4e8a44f9189f
Von <xvtran@hotmail.com>
2023-01-20 08:43:05 -0800

    JS Easy problems

---
+++ Javascript Easy/app.js
@@ -0,0 +1,21 @@
+function addition(a, b) {
+  return a + b;
+}
+
+console.log(addition(-3, -5));
+
+function hoursIntoSeconds(hours) {
+  return hours * 3600;
+}
+
+console.log(hoursIntoSeconds(2));
+console.log(hoursIntoSeconds(10));
+console.log(hoursIntoSeconds(24));
+
+function appendFrontend(str) {
+    return str + "Frontend"
+}
+
+console.log(appendFrontend('Apple'))
+console.log(appendFrontend('Banana'))
+console.log(appendFrontend('Orange'))
--- promise.js
+++ promise.js
@@ -22,9 +22,67 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
 // 2. Async/Await
 async function main() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
-    const data = await response.json()
+    const data = await response.json()             //Only need .json when talking to a backend server
     emailRef.innerHTML = data.email
 }
 
-main()
+main();
+
+const statusRef = document.querySelector('.status')
+const videoRef = document.querySelector('.video')
+
+// To create a Promise function
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {         //return a promise
+        setTimeout(() => {
+        resolve("VIP")
+        }, 2000);
+    });
+}
+
+// 1. Then method
+getSubscriptionStatus().then(response => console.log(response))
+
+
+// 2. Async/Await method
+async function main() {
+    const status = await getSubscriptionStatus()
+    statusRef.innerHTML = status;
+}
+
+main();
+
+/**
+ * 1. Create a function called `getVideo`
+ * 2. Accept a parameter called `subscriptionStatus`
+ * 3. Return a new Promise inside of the function that:
+ *      - if VIP resolve('show video')
+ *      - if FREE resolve('show trailer')
+ *      - otherwise reject('no video')
+ * 4. console.log the result of getVideo(status) in main()
+ */
+
+function getVideo (subscriptionStatus) {
+    return new Promise((resolve, reject) => {
+        if (subscriptionStatus === 'VIP') {
+            resolve('show video')
+        }
+        else if (subscriptionStatus === 'FREE') {
+            resolve('show trailer')
+        }
+        else {reject('no video')}
+});
+}
+
+async function main2() {
+    try {
+        const status = await getVideo('VIP')
+        videoRef.innerHTML = status;
+    }
+    catch (e) {
+        videoRef.innerHTML = e;
+    }
+}
+
+main2();
 
